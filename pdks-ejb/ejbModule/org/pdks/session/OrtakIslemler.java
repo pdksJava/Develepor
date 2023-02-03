55456d838515ea2b114c2a55967aa63098bafc0f
pdksJava <javaabh@gmail.com>
2023-02-03 15:35:45 +0300

    .

--- pdks-ejb/ejbModule/org/pdks/session/OrtakIslemler.java
+++ pdks-ejb/ejbModule/org/pdks/session/OrtakIslemler.java
@@ -3699,6 +3699,8 @@ public class OrtakIslemler implements Serializable {
 		} catch (Exception e) {
 		}
 		List sicilller2 = null;
+		boolean tesisDurum = isTesisDurumu();
+
 		if (istenAyrilanEkle == false && ikinciYonetici) {
 			sicilller2 = authenticatedUser.getIkinciYoneticiPersonelSicilleri();
 			if (sicilller2 != null)
@@ -3736,7 +3738,7 @@ public class OrtakIslemler implements Serializable {
 				parametreMap.put("ekSaha3.id=", ekSaha3.getId());
 			if (ekSaha4 != null && (departman == null || departman.isAdminMi()) && (departman == null || departman.isAdminMi()))
 				parametreMap.put("ekSaha4.id=", ekSaha4.getId());
-			if (tesis != null && (sirket == null || sirket.isTesisDurumu()))
+			if (tesis != null && ((sirket == null && tesisDurum) || (sirket != null && sirket.isTesisDurumu())))
 				parametreMap.put("tesis.id=", tesis.getId());
 			if (ad.trim().length() > 0)
 				parametreMap.put("ad like", ad.trim() + "%");
