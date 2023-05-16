ceae03ff65963d24ad3f2f3e7ad59ae3219f7a40
pdksJava <javaabh@gmail.com>
2023-05-16 18:33:11 +0300

    .

--- pdks-ejb/ejbModule/org/pdks/session/OrtakIslemler.java
+++ pdks-ejb/ejbModule/org/pdks/session/OrtakIslemler.java
@@ -10550,7 +10550,6 @@ public class OrtakIslemler implements Serializable {
 		String str = getParameterKey("izinERPUpdate"), sanalPersonelAciklama = getParameterKey("sanalPersonelAciklama"), kartNoAciklama = getParameterKey("kartNoAciklama");
 		if (sanalPersonelAciklama.equals(""))
 			sanalPersonelAciklama = "Sanal Personel";
-		boolean kimlikNoGoster = false;
 
 		List<PersonelView> personelList = new ArrayList<PersonelView>(list);
 		TreeMap<String, Boolean> map = mantiksalAlanlariDoldur(personelList);
@@ -10558,8 +10557,7 @@ public class OrtakIslemler implements Serializable {
 		boolean sanalPersonel = map.containsKey("sanalPersonel"), icapDurum = map.containsKey("icapDurum"), partTimeDurum = map.containsKey("partTimeDurum");
 		boolean sutIzni = map.containsKey("sutIzni"), gebeMi = map.containsKey("gebeMi"), egitimDonemi = map.containsKey("egitimDonemi"), suaOlabilir = map.containsKey("suaOlabilir");
 		boolean emailCCDurum = map.containsKey("emailCCDurum"), emailBCCDurum = map.containsKey("emailBCCDurum"), bordroAltAlani = map.containsKey("bordroAltAlani");
-
-		boolean onaysizIzinKullanilir = map.containsKey("onaysizIzinKullanilir"), tesisDurum = map.containsKey("tesisDurum"), ikinciYoneticiIzinOnayla = map.containsKey("ikinciYoneticiIzinOnayla");
+		boolean kimlikNoGoster = map.containsKey("kimlikNoGoster"), onaysizIzinKullanilir = map.containsKey("onaysizIzinKullanilir"), tesisDurum = map.containsKey("tesisDurum"), ikinciYoneticiIzinOnayla = map.containsKey("ikinciYoneticiIzinOnayla");
 		boolean departmanGoster = map.containsKey("departmanGoster"), istenAyrilmaGoster = map.containsKey("istenAyrilmaGoster"), masrafYeriGoster = map.containsKey("masrafYeriGoster"), kartNoGoster = map.containsKey("kartNoGoster");
 		ByteArrayOutputStream baos = null;
 		Workbook wb = new XSSFWorkbook();
@@ -12747,12 +12745,14 @@ public class OrtakIslemler implements Serializable {
 					if (haftaTatiliFark != 0)
 						izinSuresi += calismaModeli.getHaftaIci();
 					puantajData.setIzinSuresi(izinSuresi);
+
 					if (!puantajData.isFazlaMesaiHesapla() && puantajData.getResmiTatilToplami() > 0)
 						resmiTatilSure = puantajData.getResmiTatilToplami();
 					double saatToplami = planlanSure + haftaTatilDigerSure - puantajData.getHaftaCalismaSuresi() + (resmiTatilEkle || resmiTatilVardiyaEkle ? resmiTatilSure - gecenAyResmiTatilSure : 0.0d);
 					puantajData.setSaatToplami(saatToplami);
 					puantajData.setUcretiOdenenMesaiSure(ucretiOdenenMesaiSure);
 					puantajData.planSureHesapla(tatilGunleriMap);
+
 					int yarimYuvarla = puantajData.getYarimYuvarla();
 					if (toplamCalismaGunSayisi == 0 && puantajData.getSaatToplami() == 0.0d) {
 						if (puantajData.getPlanlananSure() != 0.0d) {
@@ -12812,6 +12812,14 @@ public class OrtakIslemler implements Serializable {
 						}
 
 						puantajData.setDevredenSure(hesaplananDenklestirme.getDevredenSure());
+					}
+					if (!calismaModeli.isFazlaMesaiVarMi()) {
+						puantajData.setHaftaCalismaSuresi(0.0d);
+						puantajData.setUcretiOdenenMesaiSure(0.0d);
+						puantajData.setAksamVardiyaSaatSayisi(0.0d);
+						puantajData.setAksamVardiyaSayisi(0);
+						puantajData.setResmiTatilToplami(0.0d);
+						puantajData.setDevredenSure(0.0d);
 					}
 					if (personelDenklestirme.getFazlaMesaiIzinKullan() && personel.isCalisiyorGun(puantajData.getSonGun())) {
 						// TODO KISMI UCRET_ODE
@@ -14911,12 +14919,12 @@ public class OrtakIslemler implements Serializable {
 		if (basTarih != null && bitTarih != null) {
 			HashMap fields = new HashMap();
 			List<Long> tipler = null;
- 			List<String> hareketTip = new ArrayList<String>();
- 			hareketTip.add(Kapi.TIPI_KODU_GIRIS);
+			List<String> hareketTip = new ArrayList<String>();
+			hareketTip.add(Kapi.TIPI_KODU_GIRIS);
 			hareketTip.add(Kapi.TIPI_KODU_CIKIS);
 			fields.put(PdksEntityController.MAP_KEY_SELECT, "id");
 			fields.put("kodu", hareketTip);
- 			fields.put("tipi=", Tanim.TIPI_KAPI_TIPI);
+			fields.put("tipi=", Tanim.TIPI_KAPI_TIPI);
 			fields.put(PdksEntityController.MAP_KEY_SESSION, session);
 			tipler = pdksEntityController.getObjectByInnerObjectListInLogic(fields, Tanim.class);
 			if (tipler.isEmpty())
@@ -14927,7 +14935,7 @@ public class OrtakIslemler implements Serializable {
 			fields.put("pdks=", Boolean.TRUE);
 			if (!tipler.isEmpty())
 				fields.put("tipi.id", tipler);
- 			fields.put(PdksEntityController.MAP_KEY_SESSION, session);
+			fields.put(PdksEntityController.MAP_KEY_SESSION, session);
 			if (session != null)
 				fields.put(PdksEntityController.MAP_KEY_SESSION, session);
 			TreeMap<Long, KapiKGS> kapiMap = pdksEntityController.getObjectByInnerObjectMapInLogic(fields, Kapi.class, false);
