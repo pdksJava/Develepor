051b0f3bf5dc8d5973957a343d4476c3de1924d8
pdksJava <javaabh@gmail.com>
2024-01-31 16:48:47 +0300

    .

--- pdks-ejb/ejbModule/org/pdks/session/VardiyaGunHome.java
+++ pdks-ejb/ejbModule/org/pdks/session/VardiyaGunHome.java
@@ -1574,7 +1574,7 @@ public class VardiyaGunHome extends EntityHome<VardiyaPlan> implements Serializa
 					IzinTipi izinTipi = oncekiVardiyaGun.getIzin().getIzinTipi();
 					if (izinTipi.isBaslamaZamaniCalismadir()) {
 						cal.setTime(vardiyaGun.getVardiyaDate());
-						if (izinTipi.isCumaCumartesiTekIzinSaysin() == false || cal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY) {
+						if (vardiya.isHaftaTatil() == false && (izinTipi.isCumaCumartesiTekIzinSaysin() == false || cal.get(Calendar.DAY_OF_WEEK) != Calendar.SATURDAY)) {
 							yaz = false;
 							if (izinSonrasiOffDurum.length() > 0)
 								izinSonrasiOffDurum.append(", ");
