a47f6a5568285d7a94fb035700002e114d0c0a62
pdksJava <javaabh@gmail.com>
2025-03-13 18:03:34 +0300

    .

--- pdks-ejb/ejbModule/org/pdks/security/action/PdksCredentials.java
+++ pdks-ejb/ejbModule/org/pdks/security/action/PdksCredentials.java
@@ -6,6 +6,7 @@ import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
 
+
 @Name("pdksCredentials")
 @Scope(ScopeType.PAGE)
 public class PdksCredentials implements Serializable {
@@ -13,22 +14,9 @@ public class PdksCredentials implements Serializable {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -4214978519648688419L;
-	
-	
-//	private String username;
-//	
-//	public String getUsername() {
-//		return username;
-//	}
-//
-//	public void setUsername(String username) {
-//		this.username = username;
-//	}
-	
-	private boolean forgetPassword;
-	
+	private static final long serialVersionUID = 7550462984060785773L;
 
+	private boolean forgetPassword;
 
 	public boolean isForgetPassword() {
 		return forgetPassword;
@@ -37,7 +25,5 @@ public class PdksCredentials implements Serializable {
 	public void setForgetPassword(boolean forgetPassword) {
 		this.forgetPassword = forgetPassword;
 	}
-
-	
 
 }
