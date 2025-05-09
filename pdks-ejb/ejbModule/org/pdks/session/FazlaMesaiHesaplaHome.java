c42fa83dfd27c4894634447e387add62a5138d03
pdksJava <javaabh@gmail.com>
2025-05-09 16:34:58 +0300

    .

--- pdks-ejb/ejbModule/org/pdks/session/FazlaMesaiHesaplaHome.java
+++ pdks-ejb/ejbModule/org/pdks/session/FazlaMesaiHesaplaHome.java
@@ -3777,7 +3777,7 @@ public class FazlaMesaiHesaplaHome extends EntityHome<DepartmanDenklestirmeDonem
 		vGun.setFazlaMesaiOnayla(null);
 		if (vGun.getGecerliHareketler() != null || vGun.getTatil() != null)
 			ortakIslemler.hareketleriDuzenle(vGun, vGun.getGecerliHareketler() == null ? vGun.getHareketler() : vGun.getGecerliHareketler(), Boolean.TRUE);
-		if (vGun.getHareketler() != null) {
+		else if (vGun.getHareketler() != null) {
 			int adet = vGun.getHareketler().size();
 			for (Iterator iterator = vGun.getHareketler().iterator(); iterator.hasNext();) {
 				HareketKGS hareket = (HareketKGS) iterator.next();
