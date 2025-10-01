e4852827de3d555f8128823556e6ee8e18c334a9
pdksJava <javaabh@gmail.com>
2025-10-01 16:32:47 +0300

    .

--- PdksWebService/src/org/pdks/dao/impl/PdksDAOHibernate.java
+++ PdksWebService/src/org/pdks/dao/impl/PdksDAOHibernate.java
@@ -1,5 +1,10 @@
 package org.pdks.dao.impl;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
 import org.pdks.dao.PdksDAO;
 
 /**
@@ -7,6 +12,31 @@ import org.pdks.dao.PdksDAO;
  */
 public class PdksDAOHibernate extends BaseDAOHibernate implements PdksDAO {
 
- 
+	@Override
+	public boolean isExisObject(String name, String type) throws Exception {
+		Session session = getHibernateCurrentSession();
+		boolean durum = false;
+		StringBuffer sb = new StringBuffer();
+		sb.append("select name, object_id from sys.objects with(nolock)");
+		sb.append(" where name = :k and type = :t");
+		HashMap fields = new HashMap();
+		fields.put("k", name);
+		fields.put("t", type);
+		List listNew = null;
+		try {
+			SQLQuery query = session.createSQLQuery(sb.toString());
+			query.setParameter("k", name);
+			query.setParameter("t", type);
+			listNew = query.list();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+		durum = listNew != null && listNew.size() == 1;
+		return durum;
+
+	}
 
 }
