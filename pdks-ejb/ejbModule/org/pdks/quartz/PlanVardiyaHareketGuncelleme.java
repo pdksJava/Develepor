839443a2c4427a7e892fa5db93f9713269d2c8bd
pdksJava <javaabh@gmail.com>
2026-01-26 14:31:55 +0300

    .

--- pdks-ejb/ejbModule/org/pdks/quartz/PlanVardiyaHareketGuncelleme.java
+++ pdks-ejb/ejbModule/org/pdks/quartz/PlanVardiyaHareketGuncelleme.java
@@ -178,18 +178,16 @@ public class PlanVardiyaHareketGuncelleme implements Serializable {
 			cal.set(Calendar.DATE, cal.getActualMaximum(Calendar.DATE));
 			Date tarihBit = cal.getTime();
 			boolean eski = sayac < 3 || buAy == sonrakiAy;
+			String str1 = eski ? "inner" : "left";
+			String str2 = eski ? "PD." + PersonelDenklestirme.COLUMN_NAME_DURUM + " = 0" : "PD." + PersonelDenklestirme.COLUMN_NAME_ID + " is null";
 			sb.append(str + " select distinct D.* from " + DenklestirmeAy.TABLE_NAME + " D " + PdksEntityController.getSelectLOCK());
 			sb.append(" inner join " + Personel.TABLE_NAME + " P " + PdksEntityController.getJoinLOCK() + " on P." + Personel.COLUMN_NAME_ISE_BASLAMA_TARIHI + " <= :bit" + sayac + " and P." + Personel.COLUMN_NAME_SSK_CIKIS_TARIHI + " >= :bas" + sayac);
 			sb.append(" and P." + Personel.COLUMN_NAME_CALISMA_MODELI + " is not null and P." + Personel.COLUMN_NAME_SABLON + " is not null");
 			sb.append(" and (P." + Personel.COLUMN_NAME_PDKS_DURUM + " = 1 or P." + Personel.COLUMN_NAME_MAIL_TAKIP + " = 1)");
 			sb.append(" inner join " + Sirket.TABLE_NAME + " S " + PdksEntityController.getJoinLOCK() + " on S." + Sirket.COLUMN_NAME_ID + " = P." + Personel.COLUMN_NAME_SIRKET + " AND S." + Sirket.COLUMN_NAME_FAZLA_MESAI + " = 1");
-			sb.append(" " + (eski ? "inner" : "left") + " join " + PersonelDenklestirme.TABLE_NAME + " PD " + PdksEntityController.getJoinLOCK() + " on PD." + PersonelDenklestirme.COLUMN_NAME_DONEM + " = D." + DenklestirmeAy.COLUMN_NAME_ID);
+			sb.append(" " + str1 + " join " + PersonelDenklestirme.TABLE_NAME + " PD " + PdksEntityController.getJoinLOCK() + " on PD." + PersonelDenklestirme.COLUMN_NAME_DONEM + " = D." + DenklestirmeAy.COLUMN_NAME_ID);
 			sb.append(" and PD." + PersonelDenklestirme.COLUMN_NAME_PERSONEL + " = P." + Personel.COLUMN_NAME_ID);
-			sb.append(" where D." + DenklestirmeAy.COLUMN_NAME_DONEM_KODU + " = :d" + sayac + " and D." + DenklestirmeAy.COLUMN_NAME_DURUM + " = 1");
-			if (eski)
-				sb.append(" and coalesce(PD." + PersonelDenklestirme.COLUMN_NAME_DURUM + ",0) = 0");
-			else
-				sb.append(" and PD." + PersonelDenklestirme.COLUMN_NAME_ID + " is null");
+			sb.append(" where D." + DenklestirmeAy.COLUMN_NAME_DONEM_KODU + " = :d" + sayac + " and D." + DenklestirmeAy.COLUMN_NAME_DURUM + " = 1 and " + str2);
 			str = " union";
 			fields.put("bas" + sayac, tarihBas);
 			fields.put("bit" + sayac, tarihBit);
