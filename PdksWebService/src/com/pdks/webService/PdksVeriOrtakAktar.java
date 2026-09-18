9bc44f36dbdb979dd35a89bb2114fbbcd9c06404
pdksJava <javaabh@gmail.com>
2026-09-18 17:15:31 +0300

    .

--- PdksWebService/src/com/pdks/webService/PdksVeriOrtakAktar.java
+++ PdksWebService/src/com/pdks/webService/PdksVeriOrtakAktar.java
@@ -4226,7 +4226,7 @@ public class PdksVeriOrtakAktar implements Serializable {
 				if (personelKGSData == null) {
 					if (iseBaslamaTarihi != null && gecmisDonem.after(iseBaslamaTarihi)) {
 						personelTanim = getTanim(null, Tanim.TIPI_ERP_TEST_PERSONEL, personelERP.getPersonelNo(), personelERP.getAdi() + " " + personelERP.getSoyadi(), dataMap, saveList);
-						if (personelTanim != null && PdksUtil.hasStringValue(personelTanim.getKodu()) == false) {
+						if (personelTanim != null && personelTanim.getKodu().equals(personelERP.getPersonelNo()) == false) {
 							personelERP.setYazildi(true);
 							personelERP.setId(-personelTanim.getId());
 							continue;
